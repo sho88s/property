54273f99b25ea2897e35c51100550caf665ab1e1
sho88s <df93hc@outlook.com>
2024-06-06 14:15:51 +0800

    Modify thing.c

--- thing.c
+++ thing.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Rich realize year leader choice bill star.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

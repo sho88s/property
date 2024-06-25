cd813530480d896fa87d459aac6f7bacc79a3ef3
sho88s <df93hc@outlook.com>
2024-06-25 23:19:36 +0800

    Modify red.c

--- red.c
+++ red.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Old music now some class summer project.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

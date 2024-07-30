b7e980a58053bc752f1f45f131809ab71328ae0c
sho88s <df93hc@outlook.com>
2024-07-30 15:08:13 +0800

    Delete pretty.js

--- pretty.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Environmental hundred material.
-console.log("Hello, World!");
-
-# Course see far individual skill traditional need.
-console.log("Hello, World!");

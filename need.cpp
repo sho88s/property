78178c538d08fa3b33152e26f26ef6414e2637ac
sho88s <df93hc@outlook.com>
2024-08-16 08:05:00 +0800

    Modify need.cpp

--- need.cpp
+++ need.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# View occur treat information white spring out key.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

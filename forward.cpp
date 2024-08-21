97b12853c2a71c6f00e427631d3087964aa3d82c
sho88s <df93hc@outlook.com>
2024-08-21 19:49:02 +0800

    Modify forward.cpp

--- forward.cpp
+++ forward.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Year involve chair.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

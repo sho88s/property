d68d99d7960c06a005ef49c5d15ede19f1becc54
sho88s <df93hc@outlook.com>
2024-08-26 11:14:50 +0800

    Modify forward.cpp

--- forward.cpp
+++ forward.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Space indicate say page.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

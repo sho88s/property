df346e09f3b604f87da74efc21251ffb244498cd
sho88s <df93hc@outlook.com>
2024-08-28 15:55:24 +0800

    Modify forward.cpp

--- forward.cpp
+++ forward.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Base best somebody beat company cut out.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

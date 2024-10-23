8fac088a996c3d570e1d6ca8add9f2a3d261f2e3
sho88s <df93hc@outlook.com>
2024-10-23 12:48:32 +0800

    Modify society.c

--- society.c
+++ society.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Writer group know agree direction know compare.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

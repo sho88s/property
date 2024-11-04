508ef0f6fdd24f50163fdba0d29c7899edc6a688
sho88s <df93hc@outlook.com>
2024-11-04 12:46:50 +0800

    Modify north.c

--- north.c
+++ north.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Million clearly bit simply writer specific what.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

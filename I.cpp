786266b2ca40f02056a308f953c812e796fcff43
sho88s <df93hc@outlook.com>
2024-11-30 23:44:52 +0800

    Modify I.cpp

--- I.cpp
+++ I.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Standard civil heart determine.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

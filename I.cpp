d3830670a5708ed6b02e70c935d91ea373f5263d
sho88s <df93hc@outlook.com>
2024-12-07 20:34:45 +0800

    Modify I.cpp

--- I.cpp
+++ I.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Right history stay yet offer travel pretty.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

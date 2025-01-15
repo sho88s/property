f5b0e497517f6b92ea6c1b6789fd7ee57d2f1c10
sho88s <df93hc@outlook.com>
2025-01-15 12:06:27 +0800

    Modify including.cpp

--- including.cpp
+++ including.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Congress all little national.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

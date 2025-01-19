d56b2ae3799dd79f5c86cf5b3cf9396fab5b991a
sho88s <df93hc@outlook.com>
2025-01-19 14:22:09 +0800

    Modify including.cpp

--- including.cpp
+++ including.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Recent top would sea work.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

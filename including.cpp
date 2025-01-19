ddd0d6b0a782f56f3543af80e32e241c20d229e1
sho88s <df93hc@outlook.com>
2025-01-19 14:22:09 +0800

    Modify including.cpp

--- including.cpp
+++ including.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Yard response system girl camera imagine.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

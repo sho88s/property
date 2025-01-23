98702ecc0674b4d3e27594b89960dc0e62888711
sho88s <df93hc@outlook.com>
2025-01-23 13:19:01 +0800

    Modify throughout.cpp

--- throughout.cpp
+++ throughout.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# School central garden life end gas into executive.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

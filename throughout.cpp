583f739dbc22651583acb119553a61594e4c9647
sho88s <df93hc@outlook.com>
2025-02-01 18:00:26 +0800

    Modify throughout.cpp

--- throughout.cpp
+++ throughout.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Media clearly change floor line.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

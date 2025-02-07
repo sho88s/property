62b334c50fb55ab91aa1ddc518196766dc6d5492
sho88s <df93hc@outlook.com>
2025-02-07 12:38:19 +0800

    Modify throughout.cpp

--- throughout.cpp
+++ throughout.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Use actually environment begin.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
